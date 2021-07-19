903292710e1dce6f430724d7bd0b47ac0ee81f07
ByKyTa <bykyta@outlook.com.vn>
2021-07-19 14:02:37 +0700

    Add project files.

---
+++ HoMinhTrung_1811062025_LAB07/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace HoMinhTrung_1811062025_LAB07
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ HoMinhTrung_1811062025_LAB07/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HoMinhTrung_1811062025_LAB07.WebApiApplication" Language="C#" %>
